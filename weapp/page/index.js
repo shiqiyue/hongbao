88ec898f47ff0e7bf1f86ff2d67610fc9fe99c63
zhuweiyou <i@zhuweiyou.com>
2018-02-27 14:41:20 +0800

    大致完成

--- weapp/page/index.js
+++ weapp/page/index.js
@@ -1,6 +1,36 @@
 /* global Page */
 
 Page({
+  data: {
+    submit: false,
+    mobile: wx.getStorageSync('mobile')
+  },
+  getHongbao (event) {
+    if (this.data.submit) {
+      return
+    }
+    const {url, mobile} = event.detail.value
+    this.setData({submit: true})
+    wx.request({
+      url: 'http://101.132.113.122:3007/hongbao',
+      data: {url, mobile},
+      method: 'POST',
+      success: ({data}) => wx.showModal({
+        title: '提示',
+        content: data.message,
+        showCancel: false
+      }),
+      fail: () => wx.showModal({
+        title: '提示',
+        content: '网络繁忙，请稍后重试',
+        showCancel: false
+      }),
+      complete: () => {
+        this.setData({submit: false})
+        wx.setStorageSync('mobile', mobile)
+      }
+    })
+  },
   onShareAppMessage () {
     return {
       title: '一键领取手气最佳红包（饿了么、美团）',
